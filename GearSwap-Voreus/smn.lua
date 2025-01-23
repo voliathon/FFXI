ad12d761d37bdd49f7d650326876df0ebde9b92e
Voli and Az <81529384+voliathon@users.noreply.github.com>
2025-01-23 03:20:50 -0500

    Add files via upload

--- GearSwap-Voreus/smn.lua
+++ GearSwap-Voreus/smn.lua
@@ -37,6 +37,7 @@ function get_sets()
     sets.AstralFlow = {}
     sets.ManaCede = { hands="Beckoner's Bracers +2"}
     sets.ElementalSiphon = {
+		legs="Beck. Spats +3",
 		feet="Beckoner's Pigaches +2",
 		back=SummonerCapes.BloodPact,
 		waist="Ligeia Sash"
@@ -102,7 +103,7 @@ function get_sets()
 		head={ name="Helios Band", augments={'Pet: Attack+29 Pet: Rng.Atk.+29','Pet: "Dbl. Atk."+7','Blood Pact Dmg.+7',}},
 		body="Beck. Doublet +2",
 		hands="Beck. Bracers +2",
-		legs="Enticer's pants",
+		legs="Beck. Spats +3",
 		feet={ name="Helios Boots", augments={'Pet: Accuracy+30 Pet: Rng. Acc.+30','Pet: "Dbl. Atk."+8','Blood Pact Dmg.+5',}},
 		neck="Empath Necklace",
 		waist="Klouskap Sash",
@@ -191,7 +192,20 @@ function get_sets()
       body="Twilight Cloak"
 	}
 
-	sets.SelfAccuracy = {}
+	sets.SelfAccuracy = {
+		head="Beckoner's Horn +3",
+		body="Beck. Doublet +2",
+		hands="Beck. Bracers +2",
+		legs="Beck. Spats +3",
+		feet="Beck. Pigaches +2",
+		neck="Sanctity Necklace",
+		waist="Klouskap Sash",
+		left_ear="Telos Earring",
+		right_ear="Digni. Earring",
+		left_ring="Varar Ring +1",
+		right_ring="Varar Ring +1",
+		back={ name="Aurist's Cape +1", augments={'Path: A',}}	
+	}
 
     sets.Movement = {left_ring="Shneddick Ring"}
 
@@ -218,7 +232,7 @@ function get_sets()
 		head="Beckoner's Horn +3",
 		body="Beck. Doublet +2",
 		hands="Lamassu Mitts +1",
-		legs="Beck. Spats +2",
+		legs="Beck. Spats +3",
 		feet="Beck. Pigaches +2",
 		neck="Caller's Pendant",
 		waist="Lucidity Sash",
